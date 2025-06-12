c8534155a9050da4314e658299ff928f61b0978a
EliAnn <rmeliud@gmail.com>
2025-06-11 19:33:30 -0500

    Initial commit: Project setup with Django and dependencies

--- generator/apps.py
+++ generator/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ContractGeneratorConfig(AppConfig):
+class GeneratorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contract_generator'
+    name = 'generator'
